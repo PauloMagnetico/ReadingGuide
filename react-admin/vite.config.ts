e68974d5c59cd4f409837bec68eb2c2a24f4c504
PauloMagnetico <laurens.vangucht@outlook.com>
2024-01-25 16:49:49 +0100

    everything running with docker-compose

--- react-admin/vite.config.ts
+++ react-admin/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host:true,
     port: 5174 // Replace 5174 with your desired port number
   },
   plugins: [react()],
