71d64030cddd88a130de27cf447310d3e6aa2d59
PauloMagnetico <laurens.vangucht@outlook.com>
2024-01-29 14:56:31 +0100

    updated Link component to TS

--- react-admin/src/components/common/Link.tsx
+++ react-admin/src/components/common/Link.tsx
@@ -1,17 +1,28 @@
+import React from "react";
 import classNames from "classnames";
 import useNavigation from "../../hooks/use-navigation";
 
+interface LinkProps {
+    to: string;
+    children: React.ReactNode;
+    className?: string;
+    activeClassName?: string;
+}
 
-
-function Link({ to, children, className, activeClassName }) {
+const Link: React.FC<LinkProps> = ({
+    to,
+    children,
+    className,
+    activeClassName
+}) => {
     const { navigate, currentPath } = useNavigation();
 
     const classes = classNames(
-        'text-blue-500', 
+        'text-blue-500',
         className,
         currentPath === to && activeClassName);
-    
-    const handleClick = (event) => {
+
+    const handleClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
         if (event.metaKey || event.ctrlKey) {
             return;
         }
