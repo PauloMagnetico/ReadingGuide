f850ed5061ec737a54e4375e3c625f929863c264
PauloMagnetico <laurens.vangucht@outlook.com>
2024-01-29 17:03:23 +0100

    updaed AvigradeShow to TS

---
+++ react-admin/src/components/AviGrade.tsx
@@ -0,0 +1,23 @@
+import Feedback from "./Feedback";
+import { AviGrade } from "../models/enums";
+import React from "react";
+
+interface AviGradeShowProps {
+    calculatedGrade: AviGrade;
+    extractedText: AviGrade;
+    showSnackBar: (message: string) => void;
+};
+
+const AviGradeShow: React.FC<AviGradeShowProps> = ({ 
+    calculatedGrade, 
+    extractedText, 
+    showSnackBar 
+}) => {
+    return (
+        <div className="mt-2 p-4 bg-gray-100 rounded-xl border-8 border-double border-palette_4 text-center">Het leesniveau is: {calculatedGrade}
+            <Feedback showSnackBar={showSnackBar} extractedText={extractedText} calculatedGrade={calculatedGrade}/>
+        </div>
+    )
+}
+
+export default AviGradeShow;
