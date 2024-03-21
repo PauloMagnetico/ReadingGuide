8fa1c4b1c551ad9f8779c78c3afe3b7f25065b22
PauloMagnetico <laurens.vangucht@outlook.com>
2024-03-21 11:51:59 +0100

    added github pages config for prod deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,15 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs';
 
+// Conditionally apply HTTPS configuration for development only,
+// in production the server will be https
+const httpsConfig = process.env.NODE_ENV === 'development' ? {
+  https: {
+    key: fs.readFileSync('server.key'), 
+    cert: fs.readFileSync('server.cert'),
+  }
+} : {};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host:true,
-    https: {
-      key: fs.readFileSync('server.key'), 
-      cert: fs.readFileSync('server.cert'),
-    },
+    ...httpsConfig,
     port: 5173
   },
-  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/ReadingGuide/' : '/',
+  plugins: [react()]
 })
