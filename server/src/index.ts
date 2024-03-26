e2db5d7a7522ec04e6316f1b703e883cea0fd9ae
PauloMagnetico <laurens.vangucht@outlook.com>
2024-03-26 14:34:37 +0100

    updated config for Prod DB

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,14 +20,14 @@ import {
 } from './controllers/feedbackController';
 import { aviGrades } from './models/enums';
 
-const dbUrl = process.env.DB_URL || 'mongodb://mongodb:27017/feedbackData'
+const dbUrl = process.env.DB_URL!
 
 mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-  console.log("Database connected");
+  console.log("Database connected", dbUrl);
 });
 
 
