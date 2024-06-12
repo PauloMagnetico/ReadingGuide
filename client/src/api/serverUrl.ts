e4fe3c0f7505aca5ae833bba6a743a2f66f727d2
PauloMagnetico <laurens.vangucht@outlook.com>
2024-06-12 18:23:12 +0200

    dynamic IP settings

--- client/src/api/serverUrl.ts
+++ client/src/api/serverUrl.ts
@@ -1,1 +1,4 @@
-export const serverUrl = import.meta.env.PROD ? import.meta.env.VITE_SERVER_URL : "https://192.168.0.120:3000";
+// const baseUrl = "https://192.168.0.120" //when wifi dongle is connected
+const baseUrl = "https://172.22.52.67" // when I use other wifi adaptar
+
+export const serverUrl = import.meta.env.PROD ? import.meta.env.VITE_SERVER_URL : `${baseUrl}:3000`;
