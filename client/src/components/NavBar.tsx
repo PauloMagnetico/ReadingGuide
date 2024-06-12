f1f338ad978e641e6111f67e2bae482c087d73fa
PauloMagnetico <laurens.vangucht@outlook.com>
2024-06-12 16:18:46 +0200

    customized colors of switch

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -3,6 +3,8 @@ import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import { useState } from 'react';
 import InfoPage from '../pages/InfoPage';
 import { GoChevronUp } from 'react-icons/go';
+import Switch from './common/Switch'
+
 
 
 // need to use react-router-dom for github pages
@@ -19,13 +21,16 @@ const NavBar = () => {
             <div className={`overflow-hidden transition-all duration-700 ease-in-out ${isExpanded ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'}`}>
                 <InfoPage />
             </div>
-            <div className='flex justify-center p-3'>
-                <div className='text-white mt-1 mr-4 cursor-pointer' onClick={handleClick}>
-                    {isExpanded ? <GoChevronUp /> : <InfoOutlinedIcon />}
+            <div className='flex justify-between p-3'>
+                <div className='flex justify-center flex-grow'>
+                    <div className='text-white mt-1 mr-4 cursor-pointer' onClick={handleClick}>
+                        {isExpanded ? <GoChevronUp /> : <InfoOutlinedIcon />}
+                    </div>
+                    <Typography className='text-white' variant='h6'>
+                        Leeswijzer
+                    </Typography>
                 </div>
-                <Typography className='text-white' variant='h6'>
-                    Leeswijzer
-                </Typography>
+                <Switch />
             </div>
         </div>
     )
